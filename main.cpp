cff76bd9a0689578f283eebf5b919fe9bdca0cb0
BigAssWeb <NikAnis2006@yandex.ru>
2025-07-11 01:15:39 +0300

    app1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED_PIN 2  // GPIO2, часто используется как выход для светодиода
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);  // Устанавливаем пин в режим вывода
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);  // Включаем светодиод
+  delay(1000);                  // Ждём 1 секунду
+  digitalWrite(LED_PIN, LOW);   // Выключаем светодиод
+  delay(1000);                  // Ждём 1 секунду
+}
